6cc8fc8f6ec12c6953fa4a027d120444fc765a5c
Jimmy Chu <jimmychu0807@gmail.com>
2019-12-03 19:50:35 +0800

    rm frontend/module

--- frontend/src/config/index.js
+++
@@ -1,5 +0,0 @@
-const configCommon = require('./common.json');
-const configEnv = require(`./${process.env.NODE_ENV}.json`);
-const config = { ...configCommon, ...configEnv };
-
-export default config;
